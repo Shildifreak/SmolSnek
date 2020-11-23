b2133b3176f24171055d3983b1d0e093de5565fe
Joram Brenz <joram.brenz@online.de>
2020-11-23 09:17:03 +0100

    added highscore, started working on smoother movement

--- toolchain.bash
+++ toolchain.bash
@@ -2,11 +2,13 @@
 
 strip-json-comments minify_config.json > tmp.json
 
-url='data:text/html;base64,'$(html-minifier-terser --config-file tmp.json index4.html | base64 -w0)
+url='data:text/html;base64,'$(html-minifier-terser --config-file tmp.json index5.html | base64 -w0)
+
+rm tmp.json
 
 echo $url
 echo
 echo $url | wc -c
 
-echo -n $url > index4.url
-echo -n $url | qrencode -o index4.png
+echo -n $url > index5.url
+echo -n $url | qrencode -o index5.png
